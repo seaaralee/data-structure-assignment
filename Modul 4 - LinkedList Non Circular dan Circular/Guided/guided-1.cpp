e070961c05c9c6da9ffa37ab8ff810ba6d96e5c5
Mutia Meilani <87632337+seaaralee@users.noreply.github.com>
2024-04-03 19:53:19 +0700

    Add files via upload

---
+++ Modul 4 - LinkedList Non Circular dan Circular/Guided/guided-1.cpp
@@ -0,0 +1,326 @@
+#include <iostream>
+
+using namespace std;
+
+// PROGRAM SINGLE LINKED LIST NON-CIRCULAR
+
+// Deklarasi struct node
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *head; // Deklarasi head
+Node *tail; // Deklarasi tail
+
+// Inisialisasi Node
+void init()
+{
+    head = NULL;
+    tail = NULL;
+}
+
+// Pengecekkan apakah linked list kosong
+bool isEmpty()
+{
+    if (head == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+    // Tambah depan
+    void insertDepan(int nilai)
+    {
+
+        // buat node baru
+        Node *baru = new Node();
+        baru->data = nilai;
+        baru->next = NULL;
+        if (isEmpty() == true)
+        {
+            head = tail = baru;
+            head->next = NULL;
+        }
+        else
+        {
+            baru->next = head;
+            head = baru;
+        }
+    }
+
+    // Tambah belakang
+    void insertBelakang(int nilai)
+    {
+        // buat node baru
+        Node *baru = new Node();
+        baru->data = nilai;
+        baru->next = NULL;
+        if (isEmpty() == true)
+        {
+            head = tail = baru;
+            head->next = NULL;
+        }
+        else
+        {
+            tail->next = baru;
+            tail = baru;
+        }
+    }
+
+    // Hitung jumlah list
+    int hitungList()
+    {
+        Node *hitung;
+        hitung = head;
+        int jumlah = 0;
+        while (hitung != NULL)
+        {
+            jumlah++;
+            hitung = hitung->next;
+        }
+        return jumlah;
+    }
+
+    // Tambah tengah
+    void insertTengah(int data, int posisi)
+    {
+        if (posisi < 1 || posisi > hitungList())
+        {
+            cout << "Posisi di luar jangkauan" << endl;
+        }
+        else if (posisi == 1)
+        {
+            cout << "Posisi bukan posisi tengah" << endl;
+        }
+        else
+        {
+            Node *baru, *bantu;
+            baru = new Node();
+            baru->data = data;
+
+            // tranversing
+            bantu = head;
+            int nomor = 1;
+            while (nomor < posisi - 1)
+            {
+                bantu = bantu->next;
+                nomor++;
+            }
+
+            baru->next = bantu->next;
+            bantu->next = baru;
+        }
+    }
+
+    // Hapus depan
+    void hapusDepan()
+    {
+        Node *hapus;
+        if (isEmpty() == false)
+        {
+            if (head->next != NULL)
+            {
+                hapus = head;
+                head = head->next;
+                delete hapus;
+            }
+            else
+            {
+                head = tail = NULL;
+            }
+        }
+        else
+        {
+            cout << "Linked list masih kosong" << endl;
+        }
+    }
+
+    // Hapus belakang
+    void hapusBelakang()
+    {
+        Node *hapus;
+        Node *bantu;
+        if (isEmpty() == false)
+        {
+            if (head != tail)
+            {
+                hapus = tail;
+                bantu = head;
+                while (bantu->next != tail)
+                {
+                    bantu = bantu->next;
+                }
+                tail = bantu;
+                tail->next = NULL;
+                delete hapus;
+            }
+            else
+            {
+                head = tail = NULL;
+            }
+        }
+        else
+        {
+            cout << "Linked list masih kosong" << endl;
+        }
+    }
+    // Hapus tengah
+    void hapusTengah(int posisi)
+    {
+        Node *hapus, *bantu, *sebelum;
+        if (posisi < 1 || posisi > hitungList())
+        {
+            cout << "Posisi di luar jangkauan" << endl;
+        }
+        else if (posisi == 1)
+        {
+            cout << "Posisi bukan posisi tengah" << endl;
+        }
+        else
+        {
+            int nomor = 1;
+            bantu = head;
+            while (nomor <= posisi)
+            {
+                if (nomor == posisi - 1)
+                {
+                    sebelum = bantu;
+                }
+                if (nomor == posisi)
+                {
+                    hapus = bantu;
+                }
+                bantu = bantu->next;
+                nomor++;
+            }
+            sebelum->next = bantu;
+            delete hapus;
+        }
+    }
+
+    // ubah depan
+    void ubahDepan(int data)
+    {
+        if (isEmpty() == 0)
+        {
+            head->data = data;
+        }
+        else
+        {
+            cout << "Linked list masih kosong" << endl;
+        }
+    }
+
+    // ubah tengah
+    void ubahTengah(int data, int posisi)
+    {
+        Node *bantu;
+        if (isEmpty() == 0)
+        {
+            if (posisi < 1 || posisi > hitungList())
+            {
+                cout << "Posisi di luar jangkauan" << endl;
+            }
+            else if (posisi == 1)
+            {
+                cout << "Posisi bukan posisi tengah" << endl;
+            }
+            else
+            {
+                int nomor = 1;
+                bantu = head;
+                while (nomor < posisi)
+                {
+                    bantu = bantu->next;
+                    nomor++;
+                }
+                bantu->data = data;
+            }
+        }
+        else
+        {
+            cout << "Linked list masih kosong" << endl;
+        }
+    }
+
+    // ubah belakang
+    void ubahBelakang(int data)
+    {
+        if (isEmpty() == 0)
+        {
+            tail->data = data;
+        }
+        else
+        {
+            cout << "Linked list masih kosong" << endl;
+        }
+    }
+
+    // Hapus list
+    void clearList()
+    {
+        Node *bantu, *hapus;
+        bantu = head;
+        while (bantu != NULL)
+        {
+            hapus = bantu;
+            bantu = bantu->next;
+            delete hapus;
+        }
+        head = tail = NULL;
+        cout << "List berhasil terhapus!" << endl;
+    }
+
+    // Tampilkan list
+    void tampil()
+    {
+        Node *bantu;
+        bantu = head;
+        if (isEmpty() == false)
+        {
+            while (bantu != NULL)
+            {
+                cout << bantu->data << " ";
+                bantu = bantu->next;
+            }
+            cout << endl;
+        }
+        else
+        {
+            cout << "Linked list masih kosong" << endl;
+        }
+    }
+
+    int main()
+    {
+        init();
+        insertDepan(3);
+        tampil();
+        insertBelakang(5);
+        tampil();
+        insertDepan(2);
+        tampil();
+        insertDepan(1);
+        tampil();
+        hapusDepan();
+        tampil();
+        hapusBelakang();
+        tampil();
+        insertTengah(7, 2);
+        tampil();
+        hapusTengah(2);
+        tampil();
+        ubahDepan(1);
+        tampil();
+        ubahBelakang(8);
+        tampil();
+        ubahTengah(11, 2);
+        tampil();
+
+        return 0;
+    }
